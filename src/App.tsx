23fa595831e8e3b64e4f3dbe6edf63fab298911e
olivinny <vinicius.gtoliv@gmail.com>
2023-01-12 01:52:18 -0300

    basic api running

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+
+import axios from 'axios';
+
+import { API_BASE_URL, API_KEY } from './constants';
 
 function App() {
+  const [playerSearch, setPlayerSearch] = useState('');
+  const [playerData, setPlayerData] = useState({
+    accountId: undefined,
+    id: undefined,
+    name: undefined,
+    profileIconId: undefined,
+    puuid: undefined,
+    revisionDate: undefined,
+    summonerLevel: undefined,
+  });
+
+  function searchPlayerBySummonerName() {
+    const APICallString = `${API_BASE_URL}/lol/summoner/v4/summoners/by-name/${playerSearch}?api_key=${API_KEY}`;
+    axios
+      .get(APICallString)
+      .then((resposta) => {
+        setPlayerData(resposta.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="container">
+        <h5>Entre com seu nome de invocador</h5>
+        <input onChange={(e) => setPlayerSearch(e.target.value)} />
+        <button
+          onClick={(e) => {
+            searchPlayerBySummonerName();
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          vai
+        </button>
+
+        {playerData.name !== undefined ? (
+          <div>
+            <br />
+            <img
+              src={`http://ddragon.leagueoflegends.com/cdn/13.1.1/img/profileicon/${playerData.profileIconId}.png`}
+              alt=""
+            />
+            <p>{playerData.name}</p>
+            <p>lvl: {playerData.summonerLevel}</p>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+export const API_KEY = 'RGAPI-f4366410-092f-4bbb-b882-724bb1183f21';
+export const API_BASE_URL = 'https://br1.api.riotgames.com';
